4e82bdf0473f83257d2175bb0dac26622c4f2400
Oleksii Chubenko <i1723@i.ua>
2023-11-24 14:18:37 +0000

    fixed TELEGRAM_REGEX

--- constants/joiRegex.js
+++ constants/joiRegex.js
@@ -2,7 +2,7 @@ const NAME_REGEX = /^[a-zA-Z .'-]+$/;
 const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{6,64}$/;
 const PHONE_REGEX =
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{0,4}[-.\s]?\d{0,9}$/;
-const TELEGRAM_REGEX = /(?: ?^@)[a-z0-9_]{5,32}$/;
+const TELEGRAM_REGEX = /(?: ?^@)[a-zA-Z0-9_]{5,32}$/;
 const YEAR_REGEX = /^\d{4,4}$/;
 const MONTH_DAY_REGEX = /^\d{2,2}$/;
 
